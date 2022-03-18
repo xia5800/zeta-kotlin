fd067fd57f2408e995b94394b72957992e84b552
xia5800 <ylwnpzzzn@163.com>
2022-03-18 14:54:33 +0800

    fix: 解决日期格式化问题

--- src/main/kotlin/org/zetaframework/core/utils/JSONUtil.kt
+++ src/main/kotlin/org/zetaframework/core/utils/JSONUtil.kt
@@ -1,9 +1,26 @@
 package org.zetaframework.core.utils
 
+import cn.hutool.core.date.DatePattern
 import cn.hutool.core.util.StrUtil
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.ObjectWriter
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.slf4j.LoggerFactory
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 /**
  * 封装Jackson得到的JSON工具类
@@ -16,7 +33,28 @@ object JSONUtil{
     private val objectMapper: ObjectMapper = ObjectMapper()
     private val objectWrite: ObjectWriter = objectMapper.writerWithDefaultPrettyPrinter()
 
+    init {
+        // 禁止:将日期写为时间戳，解决日期格式化问题
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
+        // 自定义扩展
+        val javaTimeModule = JavaTimeModule()
+        javaTimeModule.addSerializer(LocalDateTime::class.java, LocalDateTimeSerializer(DatePattern.NORM_DATETIME_FORMATTER))
+        javaTimeModule.addSerializer(LocalTime::class.java, LocalTimeSerializer(DatePattern.NORM_TIME_FORMATTER))
+        javaTimeModule.addSerializer(LocalDate::class.java, LocalDateSerializer(DatePattern.NORM_DATE_FORMATTER))
+        javaTimeModule.addSerializer(Long::class.java, ToStringSerializer.instance)
+        javaTimeModule.addSerializer(Long::class.javaObjectType, ToStringSerializer.instance)
+        javaTimeModule.addSerializer(BigDecimal::class.java, ToStringSerializer.instance)
+        javaTimeModule.addSerializer(BigInteger::class.java, ToStringSerializer.instance)
+        javaTimeModule.addDeserializer(LocalDateTime::class.java, LocalDateTimeDeserializer(DatePattern.NORM_DATETIME_FORMATTER))
+        javaTimeModule.addDeserializer(LocalTime::class.java, LocalTimeDeserializer(DatePattern.NORM_TIME_FORMATTER))
+        javaTimeModule.addDeserializer(LocalDate::class.java, LocalDateDeserializer(DatePattern.NORM_DATE_FORMATTER))
+
+        // 注册模块
+        objectMapper.registerModule(Jdk8Module())
+        objectMapper.registerModule(KotlinModule.Builder().build())
+        objectMapper.registerModule(javaTimeModule)
+    }
     /**
      * 对象转json字符串
      *
