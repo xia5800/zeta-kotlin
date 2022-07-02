cc74f0fbe90095e7a778bf5456650afcaaae6b6a
gcc <ylwnpzzzn@163.com>
2022-07-02 11:34:30 +0800

    refactor: 改为使用BCrypt方法来进行加密解密

--- src/main/kotlin/org/zetaframework/core/saToken/SecurityCryptoConfigure.kt
+++ src/main/kotlin/org/zetaframework/core/saToken/SecurityCryptoConfigure.kt
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
  * 加密配置
  * @author gcc
  */
+@Deprecated(message = "系统中已改为使用SaToken提供的BCrypt方法来进行加密解密，将在下个版本中移除该配置")
 @Configuration
 class SecurityCryptoConfigure {
 
--- src/test/kotlin/com/zeta/PasswordCryptoTest.kt
+++ src/test/kotlin/com/zeta/PasswordCryptoTest.kt
@@ -2,7 +2,6 @@ package com.zeta
 
 import cn.dev33.satoken.secure.BCrypt
 import org.junit.jupiter.api.Test
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 /**
  * 密码加密测试
@@ -15,8 +14,6 @@ class PasswordCryptoTest {
      */
     @Test
     fun cryptoPassword() {
-        // 使用 spring security 提供的 BCryptPasswordEncoder 加密密码
-        println(BCryptPasswordEncoder().encode("zetaAdmin"))
         // 使用 saToken提供的 BCrypt 加密密码
         println(BCrypt.hashpw("zetaAdmin"))
     }
@@ -26,14 +23,6 @@ class PasswordCryptoTest {
      */
     @Test
     fun comparePassword() {
-        // 使用 spring security 提供的 BCryptPasswordEncoder 解密密码
-        println(
-            BCryptPasswordEncoder().matches(
-                "zetaAdmin",
-                "\$2a\$10\$1aXDYeBpVRaTPr7Fd2tY1.u43r.Uj5t1eyQ5iIySryuht2lv4Oauy"
-            )
-        )
-
         // 使用 saToken提供的 BCrypt 解密密码
         println(BCrypt.checkpw(
             "zetaAdmin",
